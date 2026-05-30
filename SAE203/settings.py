d6afebb9b0e25d2253da09cb1918c9591f7b0eb7
Patpat200 <adilelb2007@gmail.com>
2026-05-30 10:40:39 +0200

    Update database configuration and modify URL routing

--- SAE203/settings.py
+++ SAE203/settings.py
@@ -74,28 +74,28 @@ WSGI_APPLICATION = 'SAE203.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trafic_aerien',
+        'USER': 'django_user',
+        'PASSWORD': 'toto',
+        'HOST': '192.168.1.80',
+        'PORT': '3306',
+    }
+}
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'trafic_aerien',
-#         'USER': 'django_user',
-#         'PASSWORD': 'django1234',
+#         'NAME': 'trafic_aerien', 
+#         'USER': 'root',
+#         'PASSWORD': '',  # Laisse vide si tu es sur XAMPP/WAMP
 #         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'trafic_aerien', 
-        'USER': 'root',
-        'PASSWORD': '',  # Laisse vide si tu es sur XAMPP/WAMP
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
 
 
 
