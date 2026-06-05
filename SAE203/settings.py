bdee61764e3f0dbe389b7992f679943cd3dae4d8
Patpat200 <adilelb2007@gmail.com>
2026-06-05 17:57:02 +0200

    changement des IP pour facilité le deploiement

--- SAE203/settings.py
+++ SAE203/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0b$bwzsa*tse_skpk94+nr1b-!)&&gkwz4f0oq9@w(i5c80j+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.1.86']
 
 
 # Application definition
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'trafic_aerien',
         'USER': 'django_user',
         'PASSWORD': 'toto',
-        'HOST': '192.168.56.10',
+        'HOST': '192.168.1.54',
         'PORT': '3306',
     }
 }
